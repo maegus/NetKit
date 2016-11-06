ba3e5aadd304a3becbed522a3b5c334706ae9370
Draveness <stark.draven@gmail.com>
2016-11-06 15:06:42 +0800

    Add NKHTTPRequestTests

--- NetKit/NKHTTPRequest.h
+++ NetKit/NKHTTPRequest.h
@@ -10,4 +10,6 @@
 
 @interface NKHTTPRequest : NSObject
 
+
+
 @end
---
+++ NetKitTests/NKHTTPRequestTests.m
@@ -0,0 +1,26 @@
+//
+//  NKHTTPRequestTests.m
+//  NetKit
+//
+//  Created by Draveness on 11/6/16.
+//  Copyright © 2016 Draveness. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NKHTTPRequestTests : XCTestCase
+
+@end
+
+@implementation NKHTTPRequestTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+
+@end
