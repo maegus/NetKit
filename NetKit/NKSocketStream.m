4c128e843fe5f41af878827615cd3d48f4622e03
Draveness <stark.draven@gmail.com>
2016-11-06 16:39:17 +0800

    Add NKSocketStream file

---
+++ NetKit/NKSocketStream.m
@@ -0,0 +1,13 @@
+//
+//  NKSocketStream.m
+//  NetKit
+//
+//  Created by Draveness on 11/6/16.
+//  Copyright © 2016 Draveness. All rights reserved.
+//
+
+#import "NKSocketStream.h"
+
+@implementation NKSocketStream
+
+@end
